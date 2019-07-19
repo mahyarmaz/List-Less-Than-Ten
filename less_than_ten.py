343c70dca6fe1b147585e658a00b25523075d88f
mahyarmaz <52723120+mahyarmaz@users.noreply.github.com>
2019-07-19 14:06:10 -0400

    Add files via upload

---
+++ less_than_ten.py
@@ -0,0 +1,10 @@
+list_name = []
+list_name = (input('Please enter a list of numbers separated by space: '))
+list = list_name.split()
+new_list = []
+for i in list:
+    if int(i) < 10:
+        new_list.append(i)
+print(f'Your list of numbers below 10 are : {new_list}')
+
+
